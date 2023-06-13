a9ef68ec9b6777619767512cac33c777f813932e
abusohib <abushoaib73@gmail.com>
2023-06-13 22:31:11 +0600

    gitignore files added

--- config.php
+++ config.php
@@ -3,8 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 if(!defined('BASE_URL'))
-    define('BASE_URL', 'http://localhost/spa_app/');
+     define('BASE_URL', 'http://localhost/ava/spa_app/');
 
 
 if(!defined('BASE_PATH'))
-    define('BASE_PATH', __DIR__);
+     define('BASE_PATH', __DIR__);
